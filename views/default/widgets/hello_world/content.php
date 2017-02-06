5c97527a928038945024b4859420392864f39ee4
n3cs1s <pavel.primavera@gmail.com>
2017-02-06 13:04:56 +0200

    Update content.php

--- views/default/widgets/hello_world/content.php
+++ views/default/widgets/hello_world/content.php
@@ -2,7 +2,7 @@
 
 echo "<h1>Hello world</h1>";
 
-$spider = new Spider('http://www.dmoz.org');
+/*$spider = new Spider('http://www.dmoz.org');
 $spider->getDiscovererSet()->set(new XPathExpressionDiscoverer("//div[@id='catalogs']//a"));
 $spider->getDiscovererSet()->maxDepth = 1;
 $spider->getQueueManager()->maxQueueSize = 10;
@@ -27,3 +27,4 @@ echo "<br />\n\nDOWNLOADED RESOURCES: ";
 foreach ($spider->getDownloader()->getPersistenceHandler() as $resource) {
     echo "<br />\n - " . $resource->getCrawler()->filterXpath('//title')->text();
 }
+*/
