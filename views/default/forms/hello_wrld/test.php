bdccfd86580c24c43678d004e873f81ce8d2cd1c
n3cs1s <pavel.primavera@gmail.com>
2017-02-06 16:26:23 +0200

    Update test.php

--- views/default/forms/hello_wrld/test.php
+++ views/default/forms/hello_wrld/test.php
@@ -2,5 +2,5 @@
 
 
 $title = elgg_extract('title', $vars, '');
-echo elgg_view('input/text', array('name' => 'title', 'value' => $title));
+echo elgg_view('input/text', array('name' => 'title', 'value' => $title,'class'=>'hello_wrld'));
 
