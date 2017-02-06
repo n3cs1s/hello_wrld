1a1d1623dac7fb92385141ed01467f35a2692340
n3cs1s <pavel.primavera@gmail.com>
2017-02-06 16:35:37 +0200

    Update content.php

--- views/default/widgets/hello_world/content.php
+++ views/default/widgets/hello_world/content.php
@@ -15,7 +15,10 @@ print_r($objs);
 
 ?>
 <script>
-    elgg.get('ajax/form/hello_wrld/test', {
+    elgg.get('ajax/form/hello_wrld/test',{
+  data: {
+    guid: 123 // querystring
+  },
   success: function (output) {
     $('.hello_wrld').html(output);
   }
